f482369259db76c3ad7695161621efd845cda991
labsfiap <labsfiap@L2603MICRO59>
2024-09-04 19:58:24 -0300

    Add project files.

---
+++ FiapFolhaPagamento/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FiapFolhaPagamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+          
+            // Criar uma estrutura de dados para representar a tabela 
+            // Calcular o desconto progressivo por faixa
+            // Somar os descontos de cada faixa
+        
+        }
+    }
+}
