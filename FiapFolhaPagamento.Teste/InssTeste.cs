4a7df38b7486a5573a486386bfc65d700356c8c8
Pepwt <pa632004@gmail.com>
2024-09-04 20:51:39 -0300

    Verificando o teste

---
+++ FiapFolhaPagamento.Teste/InssTeste.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapFolhaPagamento.Teste
+{
+    public class InssTeste
+    {
+        [Fact]
+        public void Deveria_Aplicar_Faixa_1()
+        {
+            // Arrange: configurar para o teste
+            var inss = new Inss();
+            //1412.00 - 7.5% = 105.9
+            var desconto = inss.CalcularDesconto(1412);
+
+            //Assert: verifica se a asserção 
+            Assert.Equal(105.9m, desconto);
+        }
+    }
+}
