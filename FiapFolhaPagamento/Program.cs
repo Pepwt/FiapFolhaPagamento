ef171e5b9370bbbd6204427c7d388b8c1d41df7b
Pepwt <pa632004@gmail.com>
2024-09-04 22:01:24 -0300

    Criando o teste INSSFaixaTeste

--- FiapFolhaPagamento/Program.cs
+++ FiapFolhaPagamento/Program.cs
@@ -20,38 +20,44 @@
 
         public decimal Aliquota { get; set; }
 
-
+        public bool ContemValor(decimal valor)
+        {
+            if (valor > Teto)
+                return true;
+            return valor >= Teto;
+        }
     }
 
-    public class Inss
-    {
-        public Inss()
+        public class Inss
         {
-            Faixas = new List<INSSFaixa>
+            public Inss()
+            {
+                Faixas = new List<INSSFaixa>
             {
                 new INSSFaixa {  Piso = 0, Teto = 1412, Aliquota = 7.5m},
                 new INSSFaixa { Piso = 1412.01m, Teto = 2666.68m, Aliquota = 9 },
                 new INSSFaixa { Piso = 2666.69m, Teto = 4000.03m, Aliquota = 12 },
                 new INSSFaixa { Piso = 4000.03m, Teto = 7786.02m, Aliquota = 14 }
         };
-       }
-
-        public List<INSSFaixa> Faixas { get; set; }
-
-        public decimal CalcularDesconto(decimal salarioBruto)
-        {
-            decimal desconto = 0m;
-
-            foreach (var item in Faixas)
-            {
-                if (salarioBruto >= item.Piso && salarioBruto <= item.Teto)
-                {
-                    desconto = salarioBruto * item.Aliquota / 100;
-                    return desconto;
-                }
             }
-            return 0;
+
+            public List<INSSFaixa> Faixas { get; set; }
+
+            public decimal CalcularDesconto(decimal salarioBruto)
+            {
+                decimal desconto = 0m;
+
+                foreach (var item in Faixas)
+                {
+                    if (salarioBruto >= item.Piso && salarioBruto <= item.Teto)
+                    {
+                        desconto = salarioBruto * item.Aliquota / 100;
+                        return desconto;
+                    }
+                }
+                return 0;
+            }
         }
     }
-}
+
 
